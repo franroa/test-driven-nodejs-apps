fa053464f0b495d6625700c0f8324a28ff33a5e8
franroa <franciscoroaprieto@gmail.com>
2017-08-07 22:33:03 +0200

    dockerfile

--- kickstart.sh
+++ kickstart.sh
@@ -4,5 +4,8 @@ docker rm -f $(docker ps -a -q)
 
 docker build -t javascript-tdd-environment .
 
-docker run --name js-tdd -ti -p 8080:8080 -v "$PWD:/myproject" javascript-tdd-environment /bin/bash
+docker run -d -P --name js-tdd javascript-tdd-environment
+
+
+# docker run --name js-tdd2 -d -p 8080:8080 -p 22:22 -v "$PWD:/myproject" javascript-tdd-environment
 
