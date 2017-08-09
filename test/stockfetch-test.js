9a8337fdebe4eb8098a48287b42aa6ba31feb630
franroa <franciscoroaprieto@gmail.com>
2017-08-09 08:11:44 +0200

    stockfetch

--- test/stockfetch-test.js
+++ test/stockfetch-test.js
@@ -255,4 +255,83 @@ describe('Stockfetch tests', function() {
         printReportMock.verify();
     });
 
+    it('printReport should send price, errors once all responses arrive',
+        function() {
+            stockfetch.prices = { 'GOOG': 12.34 };
+            stockfetch.errors = { 'AAPL': 'error' };
+            stockfetch.tickersCount = 2;
+
+            var callbackMock =
+                sandbox.mock(stockfetch)
+                    .expects('reportCallback')
+                    .withArgs([['GOOG', 12.34]], [['AAPL', 'error']]);
+
+            stockfetch.printReport();
+            callbackMock.verify();
+        });
+
+    it('printReport should not send before all responses arrive', function() {
+        stockfetch.prices = { 'GOOG': 12.34 };
+        stockfetch.errors = { 'AAPL': 'error' };
+        stockfetch.tickersCount = 3;
+
+        var callbackMock = sandbox.mock(stockfetch)
+            .expects('reportCallback')
+            .never();
+
+        stockfetch.printReport();
+        callbackMock.verify();
+    });
+
+
+    it('printReport should call sortData once for prices, once for errors',
+        function() {
+            stockfetch.prices = { 'GOOG': 12.34 };
+            stockfetch.errors = { 'AAPL': 'error' };
+            stockfetch.tickersCount = 2;
+
+            var mock = sandbox.mock(stockfetch);
+            mock.expects('sortData').withArgs(stockfetch.prices);
+            mock.expects('sortData').withArgs(stockfetch.errors);
+
+            stockfetch.printReport();
+            mock.verify();
+        });
+
+    it('sortData should sort the data based on the symbols', function() {
+        var dataToSort = {
+            'GOOG': 1.2,
+            'AAPL': 2.1
+        };
+
+        var result = stockfetch.sortData(dataToSort);
+        expect(result).to.be.eql([['AAPL', 2.1], ['GOOG', 1.2]]);
+    });
+
+    it('getPriceForTickers should report error for invalid file',
+        function(done) {
+            var onError = function(error) {
+                expect(error).to.be.eql('Error reading file: InvalidFile');
+                done();
+            };
+            var display = function() {};
+
+            stockfetch.getPriceForTickers('InvalidFile', display, onError);
+        });
+
+    it('getPriceForTickers should respond well for a valid file',
+        function(done) {
+            var onError = sandbox.mock().never();
+
+            var display = function(prices, errors) {
+                expect(prices.length).to.be.eql(4);
+                expect(errors.length).to.be.eql(1);
+                onError.verify();
+                done();
+            };
+
+            this.timeout(10000);
+
+            stockfetch.getPriceForTickers('mixedTickers.txt', display, onError);
+        });
 });
